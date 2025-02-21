2f3c2da7f8456667ff46548b40321af2e8d250e4
Jovytta <jovyttayauwanta@gmail.com>
2025-02-21 20:08:48 +0800

    Initial Commit

---
+++ FruitSlasher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FruitSlasher
+//
+//  Created by Mac12 on 2025/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FruitSlasher/FruitSlasherApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitSlasherApp.swift
+//  FruitSlasher
+//
+//  Created by Mac12 on 2025/2/21.
+//
+
+import SwiftUI
+
+@main
+struct FruitSlasherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
